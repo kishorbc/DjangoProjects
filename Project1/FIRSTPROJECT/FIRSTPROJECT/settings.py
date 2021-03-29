edafffe58fb04084370b152038660bafb96bf0b3
kishorbc <74278311+kishorbc@users.noreply.github.com>
2021-03-29 09:05:29 -0500

    Update settings.py

--- Project1/FIRSTPROJECT/FIRSTPROJECT/settings.py
+++ Project1/FIRSTPROJECT/FIRSTPROJECT/settings.py
@@ -20,7 +20,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'z^l&7*-+*wuksswwnb6an%&%q230dwc@%s66pr9hk&)3ol03%k'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
